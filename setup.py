0fe298ff8511be7a84552f384f03cd58c87cbc95
ivan-kryukov <ivan.o.kryukov@gmail.com>
2013-06-12 14:38:21 -0600

    stable

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup
 setup(name='pypt',
 	version='0.1',
-	description="A simple tool to manage your data workflow",
+	description="A simple project template tool to manage your data workflow",
 	author="Ivan Kryukov",
 	license="WTFUW",
-	scripts = {"pypt=py-make:main"},
+	scripts = ["make.py"],
 	py_modules=['pypt'],
 )
