ed6042134cff3ccb2646c5cffb1c6dfb57820c94
ivan-kryukov <ivan.o.kryukov@gmail.com>
2013-06-13 11:45:40 -0600

    back with minor changes

--- pypt/__init__.py
+++ pypt/__init__.py
@@ -2,6 +2,6 @@
 from envoy import run
 #internal
 from .make import task,build,_tasks_description,_run_by_task_name
-from .pypt import render,run
+from .pypt import render
 
 __all__=["run","task","build","render"]
